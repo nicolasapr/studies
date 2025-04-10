266d80703d880f93998590ef2c35da5b9391caa7
nicolasapr <70151304+nicolasapr@users.noreply.github.com>
2025-04-10 00:44:28 +0000

    Interative dog

--- Python/Test_POO.py
+++ Python/Test_POO.py
@@ -18,10 +18,29 @@ class dog:
     def older(self):
         self.age += 1
         self.timeWF += 1
+        self.hungry = True
+        if self.age > 13:
+            self.live = False
+            print(f"{self.name} is dead")
 
         print(f"{self.name} is now {self.age} years old")
     def timeWithoutFood(self):
         if self.timeWF >= 3:
             self.live = False
             print(f"{self.name} is dead")
+#little game
+dog1 = dog(input("Enter the name of your dog: "), int(input("Enter the age of your dog: ")), False, True, 0)
+
+while dog1.live:
+    dog1.info()
+    action = input("What you dog will do? (eat, bark, older): ").lower().strip()
+    if action == "eat":
+        dog1.eat()
+    elif action == "bark":
+        dog1.bark()
+    elif action == "older":
+        dog1.older()
+    else:
+        print("Invalid action")
+    dog1.timeWithoutFood()
     
