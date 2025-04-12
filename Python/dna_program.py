0f55832d8f649b730efe88a552c906f1189d89d4
nicolasapr <70151304+nicolasapr@users.noreply.github.com>
2025-04-12 00:45:51 +0000

    DNA_genome_undone

---
+++ Python/dna_program.py
@@ -0,0 +1,48 @@
+testes = []
+contador = 0
+while True:
+    k, n = map(int, input().split())
+    count = 0
+    countcomplement = 0
+    if k == 0 and n == 0:
+        break
+    contador += 1
+    t = input()
+    ptransformed = ""
+    p = input()
+    for i in p[::-1]:
+        if i == "A":
+            ptransformed += "T"
+        elif i == "T":
+            ptransformed += "A"
+        elif i == "C":
+            ptransformed += "G"
+        elif i == "G":
+            ptransformed += "C"
+
+    #funcão importante
+    for i in range(len(p)):
+        if p[i:len(p)].startswith(t):
+            count += 1
+    for i in range(len(ptransformed)):
+        if ptransformed[i:len(ptransformed)].startswith(t):
+            countcomplement += 1
+
+    # Armazenar os resultados em uma lista de dicionários
+    testes.append({
+        "teste": contador,
+        "direta": count,
+        "complementar": countcomplement
+    })
+
+# Exibir os resultados
+for i in testes:
+    print(f"Teste {i['teste']}")
+    print(f"ocorrencia direta: {i['direta']}")
+    print(f"ocorrencia complementar invertida: {i['complementar']}")
+    print()
+
+
+
+
+
