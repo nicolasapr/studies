c1c1f7d9eefa15d3f0d469ab4f4dbb8218179440
nicolasapr <70151304+nicolasapr@users.noreply.github.com>
2025-04-18 23:48:42 -0300

    Formation fixed; Test OK

--- AI/Benchmark_Tinyllama.py
+++ AI/Benchmark_Tinyllama.py
@@ -2,6 +2,8 @@ import ollama
 import pandas as pd
 import time
 
+#Respostas incoerentes
+
 try:
     df_benchmark = pd.read_csv("csqa_dev_processed.csv")
     df_benchmark = df_benchmark.dropna()
@@ -20,15 +22,20 @@ def model(question, choices):
     )
     return response['message']['content'].strip()
 
-def make_prompt(question, choices):  
-    return f"""You are taking a multiple choice test. For the following question, select the EXACT text of the best answer from the given choices.
+def make_prompt(question, choices):     
+    return f"""You are taking a multiple choice test. For the following question, select the EXACT LETTER of the best answer from the given choices.
 
 Question: {question}
 
 Available choices:
-{choices}
+A: {choices[0].strip()}
+B: {choices[1].strip()}
+C: {choices[2].strip()}
+D: {choices[3].strip()}
+E: {choices[4].strip()}
 
-Important: Your response must be EXACTLY one of the choices provided above, with no additional text or explanation. Copy and paste the correct choice."""
+
+Important: Your response must be EXACTLY one of the choices alternative provided above, with no additional text or explanation. Copy and paste the correct LETTER."""
 
 def evaluate_model(model, df):
     results = []
@@ -37,7 +44,9 @@ def evaluate_model(model, df):
             question = row['question']
             choices = row['choices']
             answer = row['answer_text']
-            response = model(question, choices)
+            choices_format = choices.replace("[", "").replace("]", "").replace("'", "").split(",")
+            response = model(question, choices_format)
+            
             
             #only for debug
             print(f"\nQuestion {index + 1}: {question}")
